5460faca92f5b485712822645e8ff21fa062c7cf
billchen <cgb1021@gmail.com>
2016-04-12 18:23:04 +0800

    更换setcookie的域名会有副作用

---
+++ user_files/cache/c_2130706433146045449395.php
@@ -0,0 +1,2 @@
+<?php
+a:4:{s:11:"create_time";i:1460454493;s:11:"update_time";i:1460454493;s:6:"expire";i:360;s:5:"value";s:4:"813z";}
---
+++ user_files/cache/c_2130706433146045497678.php
@@ -0,0 +1,2 @@
+<?php
+a:4:{s:11:"create_time";i:1460454976;s:11:"update_time";i:1460454976;s:6:"expire";i:360;s:5:"value";s:4:"1jnl";}
