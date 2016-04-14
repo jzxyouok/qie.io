8e6450840a1ee7abe105f571c32805508440ce54
billchen <cgb1021@gmail.com>
2016-04-14 18:22:11 +0800

    修改app.php

--- server/core/App.php
+++ server/core/App.php
@@ -18,7 +18,9 @@ class App {
 			if(!class_exists('Loader'))
 				require(APP_PATH.'/core/Loader.php');
 			
-			//设定环境变量
+			/*
+			 * 设定环境变量
+			 */
 			$profile = Loader::loadVar(APP_PATH.'/config/profile.php', 'profile');
 			//测试模式
 			define('TEST_MODE', isset($profile['test_mode'])?(boolean)$profile['test_mode']:false);
@@ -29,41 +31,47 @@ class App {
 				error_reporting(0);
 			}
 			
+			/*
+			 * 实现路由
+			 */
 			$route = Loader::loadVar(APP_PATH.'/config/route.php');
 			$request = Loader::load('Request', array($route));
 			$dir = $request->getDir();
+			
 			$position = 0; //系统调用的uri起始位置,调用的方法在这个位置上+1,调用的方法需要的参数在这个位置
-			$objName = ''; //自动调用的控制器名称
-			$object = null; //自动调用的控制器
+			$ctrlName = ''; //自动调用的控制器名称
+			$controller = null; //自动调用的控制器
 			$method = ''; //自动调用的控制器方法
 			$param = $position+2; //调用方法是用的参数的uri参数位置为3
 			
 			//按照访问路径加载控制器
-			if(!$objName = strtolower($request->segment($position))) {
-				$objName = 'main'; //默认控制器，也就是网站首页
+			if(!($ctrlName = strtolower($request->segment($position)))) {
+				$ctrlName = 'main'; //默认控制器，也就是网站首页
 				$method = 'index';
 				$param = -1;
 			}
-			$objName{0} = strtoupper($objName{0});
-			$object = Loader::load('controller'.$dir.'/'.$objName.'Ctrl', array($process_start), false);
-			if(!$object)
-				$this->error('找不到对象::object not found', $request);
+			$ctrlName{0} = strtoupper($ctrlName{0});
+			$controller = Loader::load('controller'.$dir.'/'.$ctrlName.'Ctrl', array($process_start), false);
+			if(!$controller)
+				$this->error('找不到控制器对象::controller not found', $request);
 			
 			if(empty($method) && !($method = $request->segment($position+1))) {
 				$method = 'index'; //尝试加载默认方法
 				$param = -1;
 			}
-			if(!method_exists($object, $method)) {
+			if(!is_callable(array($controller, $method))) {
 				$method = 'index';
-				if(!method_exists($object, $method))
-					$this->error('找不到方法::method not found', array($request, $object)); //错误处理，找不到对象方法。
+				if(!is_callable(array($controller, $method)))
+					$this->error('找不到对象方法::method not found', array($request, $controller)); //错误处理，找不到对象方法。
+				
 				$param = $position + 1;
 			}
-			$object->setParamPos($param);
+			$controller->setParamPos($param);
+			
 			if($param != -1 && NULL !== ($param = $request->segment($param)))
-				$object->$method($param);
+				$controller->$method($param);
 			else
-				$object->$method();
+				$controller->$method();
 		} catch(Exception $error) {
 			$this->error('Exception:'.$error->getMessage());
 		}
