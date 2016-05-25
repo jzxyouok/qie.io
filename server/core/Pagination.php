60912444bdc300406aa0491d3250ef48ea53be24
billchen <cgb1021@gmail.com>
2016-05-25 13:19:07 +0800

    pagination 优化

--- server/core/Pagination.php
+++ server/core/Pagination.php
@@ -17,7 +17,7 @@ class Pagination {
 	private $size = 10; //每页显示数
 	private $start = 0; //数据开始位置
 	public $leftSize = 5; //左边显示页数
-	public $rightSize = 2; //右边显示页数
+	public $rightSize = 3; //右边显示页数
 	public $currentClass = 'current'; //但前页样式
 	public $etcString = '<span class="etc">...</span>'; //隐藏内容html格式
 	public $startText = '&lt;&lt;'; //第一页
@@ -27,18 +27,21 @@ class Pagination {
 	public $queryString = ''; //参数
 	public $uri = '';
 	public $style = 'default'; //分页样式
-	//private $isQuery = false;
 	
 	/*
-	 * @param int $total 总数
-	 * @param int $size 每页显示多少
-	 * @param int $current 当前页数
-	 * @param int $page 获取页数的参数，即?page=
+	 * 根据config初始化
+	 *
+	 * @param array $config array('total'=>int, 'size'=>int, 'current'=>int, 'uri'=>string, 'is_query' => boolean, 'page_flag' => string)
 	 */
 	function __construct($config = array()) {
 		if(!empty($config))
 			$this->init($config);
 	}
+	/*
+	 * 根据config初始化
+	 *
+	 * @param array $config array('total'=>int, 'size'=>int, 'current'=>int, 'uri'=>string, 'is_query' => boolean, 'page_flag' => string)
+	 */
 	public function init($config = array()) {
 		if($config) {
 			//数据
@@ -47,30 +50,26 @@ class Pagination {
 			$this->current = (int)$config['current'];
 			//跳转地址
 			$this->uri = $config['uri'] or $this->uri = $_SERVER['REQUEST_URI'];
-			//$config['is_query'] = true;
-			//$config['query_flag'] = 'pg';
-			//$config['uri'] = '/abc.php';
 			if(!$config['is_query']) {
 				if('/' != $this->uri{strlen($this->uri)-1})
 					$this->uri .= '/';
 				$this->queryString = '/'.($_SERVER['QUERY_STRING']?'?'.$_SERVER['QUERY_STRING']:'');
 			} else {
 				if(!empty($_SERVER['QUERY_STRING'])) {
-					$this->query = array();
-					$querys = explode('&',$_SERVER['QUERY_STRING']);
-					foreach($querys as $v) {
+					$querys = array();
+					$res = explode('&',$_SERVER['QUERY_STRING']);
+					//query_string去重，过滤page_flag
+					foreach($res as $v) {
 						$tmp = explode('=', $v);
-						if(!empty($tmp[0]) && !isset($this->query[$tmp[0]]) && $tmp[0] != $config['query_flag']) {
-							$this->query[$tmp[0]] = $tmp[0].'='.$tmp[1];
+						if(!empty($tmp[0]) && !isset($querys[$tmp[0]]) && $tmp[0] != $config['page_flag']) {
+							$querys[$tmp[0]] = $tmp[0].'='.$tmp[1];
 						}
 					}
-					if(!empty($this->query))
-						$this->query = '&'.implode('&', $this->query);
-					else
-						$this->query = '';
+					if(!empty($querys))
+						$this->queryString = '&'.implode('&', $querys);
 				}
 				
-				$this->uri .= (false !== strpos($this->uri, '?') ? '&' : '?').$config['query_flag'].'=';
+				$this->uri .= (false !== strpos($this->uri, '?') ? '&' : '?').$config['page_flag'].'=';
 			}
 		}
 		
@@ -119,6 +118,7 @@ class Pagination {
 	 */
 	public function setMax($max = 0) {
 		$this->max = $max;	
+		$this->init();
 	}
 	/*
 	 * 获取当前页
@@ -186,9 +186,6 @@ class Pagination {
 	private function defaultStyle() {
 		$footer = '';
 		
-		$show_etc = true;//是否显示更多
-		$footer = '';
-		
 		//显示首页，前一页
 		if($this->current > 1)
 			$footer .= '<a href="' . $this->uri . '1'.$this->queryString.'" class="start" title="第一页">'.$this->startText.'</a><a href="' . $this->uri . ($this->current -1) .$this->queryString.'" class="prev" title="上一页">'.$this->prevText.'</a>';
@@ -204,24 +201,29 @@ class Pagination {
 					$footer .= '<span class="' . $this->currentClass . '" title="第' . $i . '页">' . $i . '</span>';
 			}
 		} else {
-			if($this->leftSize <=2)
+			if($this->leftSize < 3)
 				$this->leftSize = 3;
+			if($this->rightSize < 2)
+				$this->rightSize = 2;
+			
 			$leftMinSize = floor($this->leftSize/2);//左边显示最小值
-			$leftMaxSize = $this->max - $this->rightSize;//左边显示最大值
+			$leftLength = $this->max - $this->rightSize;//左边显示最大值
 			$i = 0;
 			$length = 0;
 			//显示左边
 			if($this->current <= $leftMinSize) {
 				//如果当前页在最小范围内，显示从1开始
+				//echo 'case 1';
 				$i = 1;
 				$length = $this->leftSize;
-			} else if($this->current >= ($this->max-$this->rightSize-($this->leftSize+1)/2)) {
+			} else if($this->current > ($leftLength - ($this->leftSize-$leftMinSize))) {
 				//如果当前页靠后
-				$i = $this->max-$this->rightSize-$this->leftSize;
-				$length = $leftMaxSize-1;
-				$show_etc = false; //不显示更多
+				//echo 'case 2';
+				$i = $leftLength - $this->leftSize+1;
+				$length = $leftLength;
 			} else {
 				//如果当前页在中间位置
+				//echo 'case 3';
 				$i = $this->current-$leftMinSize;
 				$length = $this->current-$leftMinSize+$this->leftSize-1;
 			}
@@ -232,10 +234,12 @@ class Pagination {
 					$footer .= '<a href="' . $this->uri . $i . $this->queryString.'" title="第' . $i . '页">' . $i . '</a>';
 			}
 			//显示更多
-			if($show_etc)
+			$i = $leftLength+1;
+			if($length < ($i-1))
 				$footer .= $this->etcString;
 			//显示右边
-			for($i = $this->max-$this->rightSize; $i <= $this->max; $i++) {
+			$length = $this->max;
+			for(; $i <= $length; $i++) {
 				if($i == $this->current)
 					$footer .= '<span class="' . $this->currentClass . '" title="第' . $i . '页">' . $i . '</span>';
 				else
