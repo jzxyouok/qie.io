8344c7bd9c3b5fe7b7f6cdf1ec5e940d5e452fc8
billchen <cgb1021@gmail.com>
2016-05-27 15:44:01 +0800

    设定时区

--- web.config.php
+++ web.config.php
@@ -3,3 +3,5 @@
 define('DOCUMENT_ROOT', dirname(__FILE__)); //文件系统根目录
 define('APP_PATH', DOCUMENT_ROOT.'/server'); //服务端根目录
 define('DATE_FORMAT', 'Y-m-d H:i:s'); //数据库保存时间格式
+//定义时区
+date_default_timezone_set('Asia/Shanghai');
